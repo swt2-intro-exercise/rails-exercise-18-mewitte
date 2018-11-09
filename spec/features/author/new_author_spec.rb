b34bd87880f018750e1c287d8b022112d0e7680c
Melvin Witte <melvin.witte@student.hpi.de>
2018-11-09 16:55:30 +0100

    Add failing test for #10

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -3,29 +3,34 @@ require 'rails_helper'
 describe "New author page", type: :feature do
 
  it "should render withour error" do
-   visit new_author_path
+   visit(new_author_path)
  end
 
   it "should have text input for first name, last name, and homepage" do
-    visit new_author_path
-    expect(page).to have_field('author[first_name]')
-    expect(page).to have_field('author[last_name]')
-    expect(page).to have_field('author[homepage]')
+    visit(new_author_path)
+    expect(page).to(have_field('author[first_name]'))
+    expect(page).to(have_field('author[last_name]'))
+    expect(page).to(have_field('author[homepage]'))
   end
 
   it "should save Alan Turing in the databse if form is submitted" do
-    visit new_author_path
-    page.fill_in 'author[first_name]', with: 'Alan'
-    page.fill_in 'author[last_name]', with: 'Turing'
-    page.fill_in 'author[homepage]', with: 'https://wikipedia.org/wiki/Alan_Turing'
+    visit(new_author_path)
+    page.fill_in('author[first_name]', with: 'Alan')
+    page.fill_in('author[last_name]', with: 'Turing')
+    page.fill_in('author[homepage]', with: 'https://wikipedia.org/wiki/Alan_Turing')
     find('input[type=submit]').click
   end
 
   it "should display an author's details on his page" do
     @alan = FactoryBot.create :author
-    visit author_path(@alan)
+    visit(author_path(@alan))
     expect(page).to have_text('Alan')
     expect(page).to have_text('Turing')
     expect(page).to have_text('https://wikipedia.org/wiki/Alan_Turing')
+  end
+
+  it "should not create an author without a last name" do
+    @alan = Author.create(first_name: 'Alan', last_name: "", homepage: 'https://wikipedia.org/wiki/Alan_Turing')
+    expect(@alan).to_not(be_valid)
   end
 end
