e9a7aa369d2635d57ae714bacc4d5940c385e1b2
Melvin Witte <melvin.witte@student.hpi.de>
2018-11-09 15:49:51 +0100

    Closes #5

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -3,4 +3,17 @@ class AuthorsController < ApplicationController
   def new
 
   end
+
+  def create
+    @author = Author.new(article_params)
+
+    @author.save
+    # redirect_to @author
+  end
+
+  private
+
+  def article_params
+    params.require(:author).permit(:first_name, :last_name, :homepage)
+  end
 end
