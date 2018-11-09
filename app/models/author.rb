1bf3f15f565ffbc467ce5a94e59c940d752dcc8d
Melvin Witte <melvin.witte@student.hpi.de>
2018-11-09 16:55:38 +0100

    Closes #10

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,8 @@
 class Author < ApplicationRecord
+  validates(:first_name, presence: true)
+  validates(:last_name, presence: true)
+  validates(:homepage, presence: true)
+
   def name
     self.first_name + ' ' + self.last_name
   end
