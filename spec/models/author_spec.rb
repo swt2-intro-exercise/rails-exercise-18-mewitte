e9cee333ff2017f3e0aba96bddacd1f38fb24e94
Melvin Witte <melvin.witte@student.hpi.de>
2018-11-09 15:21:36 +0100

    Failing test for #3

---
+++ spec/models/author_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe Author, type: :model do
+  it "should save users when entering a user" do
+    author = Author.new(first_name: 'Alan', last_name: 'Turing', homepage: 'https://wikipedia.org/wiki/Alan_Turing')
+    expect(author.first_name).to eq('Alan')
+    expect(author.last_name).to eq('Turing')
+    expect(author.homepage).to eq('https://wikipedia.org/wiki/Alan_Turing')
+  end
+end
