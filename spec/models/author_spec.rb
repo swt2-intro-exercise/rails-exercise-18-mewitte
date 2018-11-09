0b1353993af2f2f62a3bd141d1c69baadca75c76
Melvin Witte <melvin.witte@student.hpi.de>
2018-11-09 15:28:56 +0100

    Add failing test for #4

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -7,4 +7,9 @@ describe Author, type: :model do
     expect(author.last_name).to eq('Turing')
     expect(author.homepage).to eq('https://wikipedia.org/wiki/Alan_Turing')
   end
+
+  it "should return the full name on #name" do
+    author = Author.new(first_name: 'Alan', last_name: 'Turing', homepage: 'https://wikipedia.org/wiki/Alan_Turing')
+    expect(author.name).to eq('Alan Turing')
+  end
 end
