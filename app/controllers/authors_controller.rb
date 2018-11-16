2902986c1582ae2ee6dd1309dbb8f742be114dfc
Melvin Witte <melvin.witte@student.hpi.de>
2018-11-16 05:03:17 +0100

    Closes #15

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -18,6 +18,13 @@ class AuthorsController < ApplicationController
     end
   end
 
+  def destroy
+    @author = Author.find(params[:id])
+    @author.destroy
+
+    redirect_to(authors_path)
+  end
+
   def index
     @authors = Author.all
   end
