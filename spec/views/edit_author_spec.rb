75b08e0fdab225a8308e7a2a6c963a0a005d0c53
Melvin Witte <melvin.witte@student.hpi.de>
2018-11-16 04:36:00 +0100

    Add failing test for #13

--- spec/views/edit_author_spec.rb
+++ spec/views/edit_author_spec.rb
@@ -5,4 +5,28 @@ describe "Edit author page", type: :feature do
     @author = FactoryBot.create(:author)
     visit(edit_author_path(@author))
   end
+
+  it "should display the author's details" do
+    @author = FactoryBot.create(:author)
+    visit(edit_author_path(@author))
+
+    expect(page).to(have_field('author[first_name]'))
+    expect(page).to(have_field('author[last_name]'))
+    expect(page).to(have_field('author[homepage]'))
+
+    expect(find_field('author[first_name]').value).to(eq(@author.first_name))
+    expect(find_field('author[last_name]').value).to(eq(@author.last_name))
+    expect(find_field('author[homepage]').value).to(eq(@author.homepage))
+  end
+
+  it "should save the author's first name if changed" do
+    @author = FactoryBot.create(:author)
+    visit(edit_author_path(@author))
+
+    fill_in('author[first_name]', with: "Alan Mathison")
+    find('input[type="submit"]').click
+
+    @author.reload
+    expect(@author.first_name).to(eq("Alan Mathison"))
+  end
 end
